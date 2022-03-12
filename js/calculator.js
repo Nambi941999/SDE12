f67acf86e5fa30d57fbbe6ef479e45999e58cc0a
Nambi <nambi@Nambis-MacBook-Pro.local>
2022-03-12 20:11:04 +0530

    updated

---
+++ js/calculator.js
@@ -0,0 +1,8 @@
+var a = parseInt(prompt("Enter the value of a:"));
+var b = parseInt(prompt("Enter the value of b:"));
+
+var result = a+b;
+
+// alert(result);
+console.log("Addition of two numbers:"+result);
+document.write(result);
